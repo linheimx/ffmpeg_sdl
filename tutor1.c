861fb466551f519cdca5567b970454ddf8fe2644
lijian03 <lijian03@bilibili.com>
2019-06-19 12:11:53 +0800

    add tutor1

--- tutor1.c
+++ tutor1.c
@@ -1,28 +1,3 @@
-// tutorial01.c
-// Code based on a tutorial by Martin Bohme (boehme@inb.uni-luebeckREMOVETHIS.de)
-// Tested on Gentoo, CVS version 5/01/07 compiled with GCC 4.1.1
-// With updates from https://github.com/chelyaev/ffmpeg-tutorial
-// Updates tested on:
-// LAVC 54.59.100, LAVF 54.29.104, LSWS 2.1.101
-// on GCC 4.7.2 in Debian February 2015
-
-// A small sample program that shows how to use libavformat and libavcodec to
-// read video from a file.
-//
-// Use
-//
-// gcc -o tutorial01 tutorial01.c -lavformat -lavcodec -lswscale -lz
-//
-// to build (assuming libavformat and libavcodec are correctly installed
-// your system).
-//
-// Run using
-//
-// tutorial01 myvideofile.mpg
-//
-// to write the first five frames from "myvideofile.mpg" to disk in PPM
-// format.
-
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
